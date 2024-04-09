40bd878d210599352b6c5593161a63fa6db7a2ec
Marija Kuveljic <kuveljic117@gmail.com>
2024-04-09 23:16:11 +0200

    Implement simple login page.

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<form method="POST" action="{{ route('login') }}">
+    @csrf
+
+    <div>
+        <label for="email">Email</label>
+        <input id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+        @error('email')
+            <span>{{ $message }}</span>
+        @enderror
+    </div>
+
+    <div>
+        <label for="password">Password</label>
+        <input id="password" type="password" name="password" required autocomplete="current-password">
+        @error('password')
+            <span>{{ $message }}</span>
+        @enderror
+    </div>
+
+    <div>
+        <button type="submit">Login</button>
+    </div>
+</form>
