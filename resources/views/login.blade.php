aef490739549e2e85375fa8abaa22908a0ddf9c9
Marija Kuveljic <kuveljic117@gmail.com>
2024-04-12 21:37:59 +0200

    Minor fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,14 +5,14 @@
     @csrf
 
     <div>
-      <label for="email">Email</label>
+      <label class="ml-5" for="email">Email</label>
       <input id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
       @error('email')
         <span>{{ $message }}</span>
       @enderror
     </div>
 
-    <div>
+    <div class="mt-2">
       <label for="password">Password</label>
       <input id="password" type="password" name="password" required autocomplete="current-password">
       @error('password')
@@ -21,7 +21,7 @@
     </div>
 
     <div>
-      <button class="btn btn-primary" type="submit">Login</button>
+      <button class="btn btn-primary mt-2" type="submit">Login</button>
     </div>
   </form>
 @endsection
