7bd20df90fd77aa8df21b67b90689e696c8c1f8f
Marija Kuveljic <kuveljic117@gmail.com>
2024-04-12 22:21:51 +0200

    Implement logout and add a route.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\QssApiClient;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
@@ -21,15 +22,21 @@ class AuthController extends Controller
 
     $response = $this->qssApiClient->login($email, $password);
 
-    if ($response['id']) {
+    if (isset($response['token_key'])) {
       // Retrieve access token from response
       $accessToken = $response['token_key'];
 
       Session::put('access_token', $accessToken);
 
-      return view('welcome');
+      return redirect()->route('authors.index');
     } else {
         return back()->withInput()->withErrors(['email' => 'Invalid email or password.']);
     }
+  }
+
+  public function logout()
+  {
+    Session::forget('access_token');
+    return redirect()->route('login');
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,30 +5,31 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
 
-Route::get('/', function () {
+  Route::get('/', function () {
     return view('welcome');
-});
+  });
 
-Route::get('/login', function () {
+  Route::get('/login', function () {
     return view('login');
-});
+  });
+  Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+  Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
-// Route to fetch authors
-Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index');
+  // Route to fetch authors
+  Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index');
 
-// Route to add a new author
-Route::get('/authors/create', [AuthorController::class, 'create'])->name('authors.create');
-Route::post('/authors', [AuthorController::class, 'store'])->name('authors.store');
+  // Route to add a new author
+  Route::get('/authors/create', [AuthorController::class, 'create'])->name('authors.create');
+  Route::post('/authors', [AuthorController::class, 'store'])->name('authors.store');
 
-// Route to fetch a single author
-Route::get('/authors/{id}', [AuthorController::class, 'show'])->name('authors.show');
+  // Route to fetch a single author
+  Route::get('/authors/{id}', [AuthorController::class, 'show'])->name('authors.show');
 
-// Route to delete an author
-Route::delete('/authors/{id}', [AuthorController::class, 'destroy'])->name('authors.destroy');
+  // Route to delete an author
+  Route::delete('/authors/{id}', [AuthorController::class, 'destroy'])->name('authors.destroy');
 
-Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
-Route::post('/books', [BookController::class, 'store'])->name('books.store');
+  Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+  Route::post('/books', [BookController::class, 'store'])->name('books.store');
 
-Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
+  Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
